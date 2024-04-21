885854c4a5136ec0e102195e74eff46c6fae93c6
ykelly <yk.kelly.kelly@gmail.com>
2024-04-21 09:23:17 -0700

    abc

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,10 @@
-#!/usr/bin/pup
-# Install an especific version of flask (3.0.3)
-package {'flask':
-  ensure   => '3.0.3',
-  provider => 'pip3'
+# 1-install_a_package.pp
+
+package { 'python3-pip':
+  ensure => installed,
+}
+
+exec { 'install_flask':
+  command => '/usr/bin/pip3 install Flask==3.0.3',
+  unless  => '/usr/bin/pip3 show flask | grep -q "Version: 3.0.3"',
 }
