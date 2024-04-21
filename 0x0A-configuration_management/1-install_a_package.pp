37da1dbc6f9c702f83bf943c05cafce4e1ff21f7
ykelly <yk.kelly.kelly@gmail.com>
2024-04-21 13:21:57 -0700

    abc

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,3 @@
-#!/usr/bin/pup
-# Install an especific version of flask (2.1.0)
-package {'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3'
-}
+Host *
+    passwordAuthentification no
+    IdentifyFile ~/.ssh/school
