db643e4ae96e1239c6c865ef5d491d46a966fc2b
ykellyk <yk.kelly.kelly@gmail.com>
2024-09-27 01:25:42 -0700

    popp

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,18 +1,29 @@
 #!/usr/bin/python3
 """
-0-subs
+Module 0-subs
+This module contains a function that queries the Reddit API to get the number of subscribers of a subreddit.
 """
-
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """Returns the number of subscribers for a given subreddit."""
-    url = f'https://www.reddit.com/r/{subreddit}/about.json'
-    headers = {'User-Agent': 'custom_user_agent'}
+    """
+    Queries the Reddit API and returns the number of subscribers for a given subreddit.
+
+    Args:
+        subreddit (str): The subreddit to query.
+
+    Returns:
+        int: The number of subscribers, or 0 if the subreddit is invalid.
+    """
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {'User-Agent': 'my-app/0.0.1'}
     response = requests.get(url, headers=headers, allow_redirects=False)
 
+    print(f"Status Code: {response.status_code}")  
     if response.status_code == 200:
         data = response.json()
-        return data['data']['subscribers']
-    return 0
+        print(data)  
+        return data.get('data', {}).get('subscribers', 0)
+    else:
+        return 0  
